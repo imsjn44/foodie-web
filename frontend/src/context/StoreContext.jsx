57fa93229a6a83f39a4f36ee3e1eb420e1ceb67a
imsjn44 <132452073+tsujan7@users.noreply.github.com>
2025-03-15 20:58:26 +0545

    backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://foodie-web-backend.onrender.com";
   const [token, setToken] = useState("");
   const [food_lists, setFoodList] = useState(food_list);
 
