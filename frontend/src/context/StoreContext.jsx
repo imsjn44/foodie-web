ba795a9d9c9c114a2b04a224f8b5c16071537d6e
Sujan TImalsina <iamsjn44@gmail.com>
2025-03-15 17:57:48 +0545

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://foodie-web-backend.onrender.com";
   const [token, setToken] = useState("");
   const [food_lists, setFoodList] = useState(food_list);
 
